24ea1535853ef3424f58799fe14e78eb4c3e9f0c
Hemal <127820869+HemalChudasama3@users.noreply.github.com>
2024-05-29 14:39:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+
+    fontFamily: {
+      varela: ["Varela Round", "sans-serif"],
+      rubik: ["Rubik", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
